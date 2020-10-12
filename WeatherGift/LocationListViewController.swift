2b2e57cc2873bb42e72c09ce40cbd05303dee48f
Alvin Chen <chenbog@bc.edu>
2020-10-12 00:47:35 -0400

    6.1-6.5 done

--- WeatherGift/LocationListViewController.swift
+++ WeatherGift/LocationListViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import GooglePlaces
 
 class LocationListViewController: UIViewController {
 
@@ -45,6 +46,21 @@ class LocationListViewController: UIViewController {
     
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        let autocompleteController = GMSAutocompleteViewController()
+            autocompleteController.delegate = self
+
+//            // Specify the place data types to return.
+//            let fields: GMSPlaceField = GMSPlaceField(rawValue: UInt(GMSPlaceField.name.rawValue) |
+//              UInt(GMSPlaceField.placeID.rawValue))!
+//            autocompleteController.placeFields = fields
+//
+//            // Specify a filter.
+//            let filter = GMSAutocompleteFilter()
+//            filter.type = .address
+//            autocompleteController.autocompleteFilter = filter
+
+            // Display the autocomplete view controller.
+            present(autocompleteController, animated: true, completion: nil)
     }
     
     
@@ -59,6 +75,7 @@ extension LocationListViewController: UITableViewDataSource, UITableViewDelegate
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         cell.textLabel?.text = weatherLocations[indexPath.row].name
+        cell.detailTextLabel?.text = "Lat:\(weatherLocations[indexPath.row].latitude), Long:\(weatherLocations[indexPath.row].longitude)"
         return cell
     }
     
@@ -77,4 +94,36 @@ extension LocationListViewController: UITableViewDataSource, UITableViewDelegate
     
 }
 
+extension LocationListViewController: GMSAutocompleteViewControllerDelegate {
+
+  // Handle the user's selection.
+  func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
+
+    
+    let newLocation = WeatherLocation(name: place.name ?? "unknown place", latitude: place.coordinate.latitude, longitude: place.coordinate.longitude)
+    weatherLocations.append(newLocation)
+    tableView.reloadData()
+    dismiss(animated: true, completion: nil)
+  }
+
+  func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error) {
+    // TODO: handle the error.
+    print("Error: ", error.localizedDescription)
+  }
+
+  // User canceled the operation.
+  func wasCancelled(_ viewController: GMSAutocompleteViewController) {
+    dismiss(animated: true, completion: nil)
+  }
+
+  // Turn the network activity indicator on and off again.
+  func didRequestAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
+    UIApplication.shared.isNetworkActivityIndicatorVisible = true
+  }
+
+  func didUpdateAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
+    UIApplication.shared.isNetworkActivityIndicatorVisible = false
+  }
+
+}
 
